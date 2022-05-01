b80fb0a2589f6f9749230bafc8c433a03cb8508f
Ravkier <zhou_hao_qiang@163.com>
2022-05-01 23:30:41 +0800

    init 配置路由

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    title: '洛风的全栈之路',
+    description: '洛风的全栈之路',
+    themeConfig: {
+        nav: [
+            { text: '首页', link: '/' },
+            { text: '个人生涯', link: '/career/' },
+            { text: '技术积累', link: '/accumulation/' },
+            { text: 'github', link: 'https://github.com/Ravkier/Blog',  target:'_blank' },
+          ],
+          lastUpdated: '最后更新',
+    }
+  }
